8b9f48c246b7668bde9bb9893d2624c014fda412
Onur Micoogullari <204898@skane.se>
2025-02-22 15:56:49 +0100

    Set toggleterm direction to 'float'

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -5,7 +5,7 @@ return {
       -- Add any specific configuration options you need here
       size = 20, -- Default terminal size
       open_mapping = [[<c-\>]], -- Set keybinding to toggle terminal
-      direction = 'horizontal', -- Open terminal horizontally
+      direction = 'float', -- Open terminal in floating mode
     }
   end,
 }
