f3b954432c6205befbf6c6e410aad097c287e7ed
Onur Micoogullari <204898@skane.se>
2025-04-15 08:09:03 +0200

    Updated key bindings for debug adapter

--- lua/custom/plugins/dap.lua
+++ lua/custom/plugins/dap.lua
@@ -41,7 +41,7 @@ return {
     vim.keymap.set('n', '<C-F5>', dap.continue, { desc = 'Debug: Start/Continue' })
     vim.keymap.set('n', '<C-F11>', dap.step_into, { desc = 'Debug: Step Into' })
     vim.keymap.set('n', '<C-F10>', dap.step_over, { desc = 'Debug: Step Over' })
-    vim.keymap.set('n', '<C-F12>', dap.step_out, { desc = 'Debug: Step Out' })
+    vim.keymap.set('n', '<C-S-F11>', dap.step_out, { desc = 'Debug: Step Out' })
     vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
     vim.keymap.set('n', '<leader>B', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
